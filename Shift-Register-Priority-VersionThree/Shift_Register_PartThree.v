8c80710e19da27f266cc8ad2e087525aaee17605
ali-asnaashari <ali.asnaashari2000@gmail.com>
2021-12-06 14:44:43 -0800

    4Bit-Shift-Register-Priority-VersionThree

---
+++ Shift-Register-Priority-VersionThree/Shift_Register_PartThree.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+
+module Shift_Register_PartThree(clockPulse,Reset,ParallelLoad,ShiftLeft,ShiftRight,ShiftInput,Data,ShiftOutput,dataBus);
+
+
+	input clockPulse;
+   input Reset;
+   input ParallelLoad;
+   input ShiftLeft;
+   input ShiftRight;
+	input ShiftInput;
+   input [3:0] Data;
+
+	output reg ShiftOutput;
+
+	output reg [3:0] dataBus;
+
+	always @(posedge clockPulse) 
+
+		begin
+			if (Reset)
+				dataBus <= 4'b0000;
+
+			else if (ParallelLoad)
+				dataBus <= Data;
+
+			else if (ShiftLeft & ~ShiftRight) 
+				begin
+					dataBus <= {dataBus[2:0], ShiftInput};
+					ShiftOutput <= dataBus[3];
+				end
+
+			else if (ShiftRight & ~ShiftLeft) 
+				begin
+					dataBus <= {ShiftInput,dataBus[3:1]};
+					ShiftOutput <= dataBus[0];
+				end
+		end
+
+endmodule
---
+++ Shift-Register-Priority-VersionThree/Shift_Register_PartThree_TB.v
@@ -0,0 +1,96 @@
+`timescale 1ns / 1ps
+
+module Shift_Register_PartThree_TB;
+
+	// Inputs
+	reg clockPulse;
+	reg Reset;
+	reg ParallelLoad;
+	reg ShiftLeft;
+	reg ShiftRight;
+	reg ShiftInput;
+	reg [3:0] Data;
+
+	// Outputs
+	wire ShiftOutput;
+	wire [3:0] dataBus;
+
+	// Instantiate the Unit Under Test (UUT)
+	Shift_Register_PartThree uut (
+		.clockPulse(clockPulse), 
+		.Reset(Reset), 
+		.ParallelLoad(ParallelLoad), 
+		.ShiftLeft(ShiftLeft), 
+		.ShiftRight(ShiftRight), 
+		.ShiftInput(ShiftInput), 
+		.Data(Data), 
+		.ShiftOutput(ShiftOutput), 
+		.dataBus(dataBus)
+	);
+
+	initial 
+		begin
+			clockPulse = 0;
+		end
+
+	always # 50 clockPulse = ~clockPulse;  //T = 100 ns
+
+	initial begin
+
+		Reset = 1;
+		ParallelLoad = 0;
+		ShiftLeft = 0;
+		ShiftRight = 0;
+		ShiftInput = 0;
+		Data = 0;
+
+		#100;
+
+		Reset = 0;
+		ParallelLoad = 1;
+		ShiftLeft = 0;
+		ShiftRight = 0;
+		ShiftInput = 0;
+		Data = 4'b1001;
+
+		#100;
+
+		Reset = 0;
+		ParallelLoad = 0;
+		ShiftLeft = 1;
+		ShiftRight = 0;
+		ShiftInput = 1;
+		Data = 0;
+
+		#100;
+
+		Reset = 0;
+		ParallelLoad = 0;
+		ShiftLeft = 0;
+		ShiftRight = 1;
+		ShiftInput = 0;
+		Data = 0;
+
+		#100;
+
+		Reset = 0;
+		ParallelLoad = 0;
+		ShiftLeft =  1;
+		ShiftRight = 1;
+		ShiftInput = 1;
+		Data = 0;
+		
+		#100;
+
+		Reset = 0;
+		ParallelLoad = 0;
+		ShiftLeft =  1;
+		ShiftRight = 1;
+		ShiftInput = 0;
+		Data = 0;
+
+
+	end
+      
+endmodule
+
